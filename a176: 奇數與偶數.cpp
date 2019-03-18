cc41295e9316e0434fe5e4af71abfa3d04741746
hcvs7305062 <48470830+hcvs7305062@users.noreply.github.com>
2019-03-18 21:51:40 +0800

    Create a176: 奇數與偶數.cpp

---
+++ a176: 奇數與偶數.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;                        //宣告 n
+cin>>n;                       //輸出 n
+if(n%2==0)cout <<"EVEN";      //當 n/2的餘數等於0  則輸出 EVEN
+if(n%2!=0)cout <<"ODD";       //當 n/2的餘數不等於0 則輸出 ODD
+}
